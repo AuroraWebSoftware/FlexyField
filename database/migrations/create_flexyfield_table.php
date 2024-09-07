41a33936d9b283aa368bd2362a760209801e7b66
Emre Akay <emre.akay@aurorabilisim.com>
2024-09-08 00:51:55 +0300

    - bismillah

--- database/migrations/create_flexyfield_table.php
+++ database/migrations/create_flexyfield_table.php
@@ -51,7 +51,7 @@ return new class extends Migration
 
         FlexyField::dropAndCreatePivotView();
 
-        // someting TAY ssd s
+        // someting TAY ssd s asxd
 
         $exampleValue->delete();
     }
